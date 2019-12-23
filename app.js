c947bb645bfc34c5b93908851e898365c28ee111
Clement Bourgoin <c@iwzr.fr>
2019-12-23 15:52:43 +0100

    Only play sound when letter keys are pressed

--- app.js
+++ app.js
@@ -2,14 +2,33 @@
 
 class App {
   constructor() {
-    window.addEventListener('keydown', this.play.bind(this));
+    window.addEventListener('keydown', this.onKeyDown.bind(this));
     window.addEventListener('keyup', this.stop.bind(this));
+    document.addEventListener('visibilitychange', this.stop.bind(this));
 
     this.currentSound = null;
   }
 
+  onKeyDown(event) {
+    // If Esc key is press, stop sound
+    if (event.key === 'Escape') {
+      this.stop();
+      return;
+    }
+
+    // If anything that a letter key is pressed, ignore
+    if (!/^[a-z]{1}$/.test(event.key)) {
+      return;
+    }
+
+    this.play();
+  }
+
   play() {
+    // Stop current sound if any
     this.stop();
+
+    // Create new sound
     this.currentSound = new Pizzicato.Sound({
       source: 'wave',
       options: {
@@ -20,6 +39,8 @@ class App {
         attack: 0.4,
       },
     });
+
+    // Play sound
     this.currentSound.play();
   }
 
